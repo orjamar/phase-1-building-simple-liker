c082df374f9d3fe5a2b137dc7a877126d0ee11f8
Orlando Martinez <orjamar@gmail.com>
2023-06-04 12:09:47 -0500

    Lab Completed

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function () {
+  const errorModal = document.getElementById('modal');
+  const errorModalMessage = document.getElementById('modal-message');
 
-
-
+  const likeButtons = document.querySelectorAll('.like');
+  likeButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      if (button.classList.contains('activated-heart')) {
+        button.classList.remove('activated-heart');
+        button.innerHTML = EMPTY_HEART;
+      } else {
+        mimicServerCall()
+          .then(function () {
+            button.classList.add('activated-heart');
+            button.innerHTML = FULL_HEART;
+          })
+          .catch(function (error) {
+            errorModalMessage.innerText = error;
+            errorModal.classList.remove('hidden');
+            setTimeout(function () {
+              errorModal.classList.add('hidden');
+            }, 3000);
+          });
+      }
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
